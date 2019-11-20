8c6b67ca7da277f0342d24ac1ac71c0aef3b1feb
CjEspinoza <espinoza.cj3@gmail.com>
2019-11-20 23:01:44 +0800

    move files to dist folder and npm initialize

---
+++ dist/main.js
@@ -0,0 +1,20 @@
+var vid = document.getElementById("bgtech");
+vid.playbackRate = 0.5;
+
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
+
+let date = new Date();
+let yearPos = document.getElementById("getYear");
+let currYear = date.getFullYear();
+yearPos.innerHTML = currYear;
